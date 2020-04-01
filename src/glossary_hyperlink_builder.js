615420ed43722b95ccd8e0ada5b28565df0d6a00
Spencer Rosio <spencerrosio3d@gmail.com>
2020-04-01 11:48:29 -0500

    v3
    Improved text finding
    Added checkbox for overwrite styles
    Included menu for selecting multi word glossary terms

--- src/glossary_hyperlink_builder.js
+++ src/glossary_hyperlink_builder.js
@@ -4,24 +4,34 @@ var myDoc = app.activeDocument;
 main();
 function main (){
 	
-	var selecetedTextFrame = checkSelection();
-	var words = getWords(selecetedTextFrame);
-
-	//returns an array of the hyperlink destinations
-	var glossDestinations = createGlossaryDestinations(words);
+	var selectedTextFrame = checkSelection();
+	var listOfListOfWords = getWords(selectedTextFrame);
+	var overWriteStylesBool = listOfListOfWords.overWriteStyles;
 	//loop through the glossary words
-	for (x = 0; x < words.length; x++){
-		var name = words[x].contents;
+	for (var i = 0; i < listOfListOfWords.length; i++) {
+		var wordCollection = listOfListOfWords[i] instanceof Array ? listOfListOfWords[i] : [listOfListOfWords[i]];
+		// create word collection name
+		var name = "";
+		for (var a = 0; a < wordCollection.length; a++){
+			name += wordCollection[a].contents;
+			if (a < wordCollection.length-1){
+				name += " ";
+			}
+		}
+		//returns an array of the hyperlink destinations
+		var glossDestination = createGlossaryDestinations(wordCollection, name);
 		//return the reference word selected by the user
-		var referenceWord = findReferenceDestination(words, x);
+		var referenceWord = findReferenceDestination(name);
 		//if user selected skip word this section won't run
 		if (referenceWord != null){
 			//create and return destination for reference word
 			var referenceDestination = createReferenceDestination(referenceWord, name);
 			//create hyperlink from glossary word to reference word
-			hyperlinkGlossaryTerms(words[x],referenceDestination);
+			for (var k = 0; k < wordCollection.length; k++){
+				hyperlinkGlossaryTerms(wordCollection[k],referenceDestination,overWriteStylesBool);
+			}
 			//create hyperlink from reference word to glossary word
-			hyperlinkReferenceTerms(referenceWord, glossDestinations[x]);
+			hyperlinkReferenceTerms(referenceWord, glossDestination,overWriteStylesBool);
 		}
 	}
 }
@@ -39,196 +49,316 @@ function checkSelection(){
 }
 
 function getWords(selecetedTextFrame){
-	var words = selecetedTextFrame.paragraphs.everyItem().words.firstItem().getElements();
+	var words = [];
+	var paragraphs = selecetedTextFrame.paragraphs.everyItem().getElements();
+	for(var x = 0; x < paragraphs.length; x++){
+		words[x] = paragraphs[x].words.everyItem().getElements();
+	}
+	if (words[0][0].contents == "Glossary" || words[0][0].contents == "glossary"){
+		words.splice(0,1);
+	}
+	var words = getInputGlossaryWords(words);
 	return words;
 }
 
-function createGlossaryDestinations(words){
-	var message = "";
-	var hypTextDest = [];
-	for(var x = 0; x < words.length; x++){
-		var word = words[x];
-		var name = word.contents + "_gl";
-
-		try {
-			if (!myDoc.hyperlinkTextDestinations.itemByName(name).isValid){
-				hypTextDest[x] = myDoc.hyperlinkTextDestinations.add(word);
-				hypTextDest[x].name = name;
-			} else {
-				message += "\nhyperlink destination with name: " + name + " already exists... skipping";
+function getInputGlossaryWords(words){
+	var glossaryWords = [];
+	var wordCollection = [];
+	for (var x = 0; x < words.length; x++){
+		wordCollection.push(words[x][0]);
+		glossaryWords.push(wordCollection[x]);
+	}
+	var myInputWindow = new Window("dialog", "Glossary words:");
+	myInputWindow.orientation = 'column';
+	var wordGroups = myInputWindow.add("group");
+	wordGroups.orientation = 'column';
+	//..........................
+	// Create Word groups
+	//..........................
+	for (var x = 0; x < words.length; x++){
+		var wordGroup = wordGroups.add("group");
+		wordGroup.orientation = 'row';
+		wordGroup.id = x;
+		wordGroup.numWords = 1;
+		wordGroup.wordString = words[x][0].contents;
+		wordGroup.label = wordGroup.add("statictext",undefined,wordGroup.wordString);
+		wordGroup.label.characters = 35;
+		//..........................
+		// Remove word button
+		//..........................
+		wordGroup.minusBtn = wordGroup.add("button",undefined,"-");
+		wordGroup.minusBtn.enabled = false;
+		wordGroup.minusBtn.onClick = minus_btn;
+		function minus_btn (){
+			var wordGroup = this.parent;
+			wordGroup.numWords--;
+			wordGroup.wordString = "";
+			for (var i = 0; i < wordGroup.numWords; i++){
+				wordGroup.wordString += words[wordGroup.id][i].contents + " ";
 			}
-		} catch (e) {}
+			glossaryWords[wordGroup.id] = glossaryWords[wordGroup.id].splice(-1,1);
+			wordGroup.label.text = wordGroup.wordString;
+			if (wordGroup.numWords < words[wordGroup.id].length){
+				wordGroup.addBtn.enabled = true;
+			} else {
+				this.enabled = false;
+			}
+			if(wordGroup.numWords < 2){
+				this.enabled = false;
+			}
+			myInputWindow.layout.layout (true);
+		}
+		//..........................
+		// Add word button
+		//..........................
+		wordGroup.addBtn = wordGroup.add("button",undefined,"+");
+		wordGroup.addBtn.onClick = add_btn;
+		function add_btn (){
+			var wordGroup = this.parent;
+			wordGroup.numWords++
+			wordGroup.wordString = "";
+			for (var i = 0; i < wordGroup.numWords; i++){
+				wordGroup.wordString += words[wordGroup.id][i].contents + " ";
+			}
+			if (glossaryWords[wordGroup.id] instanceof Word){
+				wordCollection = [];
+				wordCollection.push(glossaryWords[wordGroup.id]);
+				wordCollection.push(words[wordGroup.id][wordGroup.numWords-1]);
+				glossaryWords[wordGroup.id] = wordCollection;
+			} else {
+				glossaryWords[wordGroup.id].push(words[wordGroup.id][wordGroup.numWords-1]);
+			}
+			wordGroup.label.text = wordGroup.wordString;
+			if(wordGroup.numWords > 1){
+				wordGroup.minusBtn.enabled = true;
+			}
+			if (wordGroup.numWords >= words[wordGroup.id].length){
+				this.enabled = false;
+			}
+			myInputWindow.layout.layout (true);
+		}
 	}
-	if (!message==""){
-		alert(message);
+	var overWriteStylesCheckbox = myInputWindow.add("checkbox",undefined,"Overwrite character styles");
+	overWriteStylesCheckbox.value = true;
+	var myButtonGroup = myInputWindow.add("group");
+	myButtonGroup.alignment = "right";
+	myButtonGroup.add("button",undefined,"OK");
+	myButtonGroup.add("button",undefined,"Cancel");
+	myInputWindow.layout.layout (true);
+	if (myInputWindow.show()==1){
+		glossaryWords.overWriteStyles = overWriteStylesCheckbox.value;
+		return glossaryWords;
+	} else {
+		exit();
 	}
-	return hypTextDest;
 }
 
-function findReferenceDestination(words, wordNum){
-	var count = 0;
-	var message = "";
-	var word = words[wordNum];
-	var name = word.contents;
-
-	app.findTextPreferences = null;
-	app.findTextPreferences.findWhat = name;
-	var found = myDoc.findText();
-	if (found.length ==0 || found.length == 1){
-		message +="\nCould not find word " + name;
-	} else {
-		for (var x = 0; x < found.length-1; x++){
-			if (found[x].toString() == "[object Text]"){
-				var parentTF = found[x].parentTextFrames[0];
-				var wordsInFrame = parentTF.words.everyItem().getElements();
-				for (var y = 0; y < wordsInFrame.length; y++){
-					var checkWord = wordsInFrame[y].contents;
-					if (checkWord.search(name) != -1){
-						found[x] = wordsInFrame[y];
-						break;
-					}
-				}
+function createGlossaryDestinations(wordCollection, name){
+	var glossaryName = name + "_gl";
+	// create hyperlink destination
+	try {
+		var increment = 1;
+		var escape = false;
+		while (escape === false){
+			if (!myDoc.hyperlinkTextDestinations.itemByName(glossaryName).isValid){
+				var glossDestinations = myDoc.hyperlinkTextDestinations.add(wordCollection[0]);
+				glossDestinations.name = glossaryName;
+				escape = true;
+			} else {
+				glossaryName += increment;
+				increment++;
 			}
 		}
+	} catch (e) {
+		alert("create glossary destination failed try");
+	}
+	return glossDestinations;
+}
+
+function findReferenceDestination(name){
+	var count = 0;
+	var message = "";
+	var namePlus = name + "[a-zA-Z0-9]*";
+
+	app.findGrepPreferences = null;
+	app.findGrepPreferences.findWhat = namePlus;
+	var found = myDoc.findGrep();
+	//if (found.length ==0 || found.length == 1){
+	if (found.length ==0){
+		message +="\nCould not find word " + name;
+	} else {
 		var escape = 0;
 		while (escape==0){
 			found[count].select();
 			app.activeWindow.zoomPercentage = app.activeWindow.zoomPercentage;
-			var refWord = getInput(found, name, count);
-			if (refWord == "SKIPWORD"){
+			var referenceWord = getInput(found, name, count);
+			if (referenceWord == "SKIPWORD"){
 				escape = 1;
 				return null;
-			} else if (refWord == "NEXTWORD"){
+			} else if (referenceWord == "NEXTWORD"){
 				count++;
-			} else if (refWord == "PREVWORD"){
+			} else if (referenceWord == "PREVWORD"){
 				count--;
 			} else {
 				escape = 1;
 			}
 		}
-		return refWord;
+		return referenceWord;
 	}
 	if (!message==""){
 		alert(message);
 	}
 }
 
-function getInput (xWord, xName, num){
-		var myInputWindow = new Window("dialog", "Hyperlinker");
-		myInputWindow.orientation = 'column';
+function getInput (found, name, count){
+	var buttonPressed = "";
+	var myInputWindow = new Window("dialog", "Hyperlinker");
+	myInputWindow.orientation = 'column';
 
-		var pageInfoGroup = myInputWindow.add("group");
-		pageInfoGroup.orientation = 'row';
-		pageInfoGroup.foundPage = pageInfoGroup.add("statictext",[0,0,500,50],xName + " found on page " + xWord[num].parentTextFrames[0].parentPage.name);
+	var pageInfoGroup = myInputWindow.add("group");
+	pageInfoGroup.orientation = 'row';
+	try {
+		var pageNum = found[count].parentTextFrames[0].parentPage.name;
+	} catch (e) {
+		var pageNum = "pasteboard";
+	}
+	pageInfoGroup.foundPage = pageInfoGroup.add("statictext",[0,0,500,50],name + " found on page " + pageNum);
 
-		var wordInfoGroup = myInputWindow.add("group");
-		wordInfoGroup.alignment = "left";
-		wordInfoGroup.foundWord = wordInfoGroup.add("statictext",undefined,xWord[num].contents);
-		wordInfoGroup.foundWord.characters = 16;
+	var wordInfoGroup = myInputWindow.add("group");
+	wordInfoGroup.alignment = "left";
+	wordInfoGroup.foundWord = wordInfoGroup.add("statictext",undefined,found[count].contents);
+	wordInfoGroup.foundWord.characters = 16;
 
-		var myButtonGroup = myInputWindow.add("group");
-		myButtonGroup.alignment = "right";
-		var myPrevButton = myButtonGroup.add("button",undefined,"Find Previous");
-		myPrevButton.onClick = prevClick;
-		if (num==0){
-			myButtonGroup.children[0].enabled = false;
-		}
-		var myNextButton = myButtonGroup.add("button",undefined,"Find Next");
-		myNextButton.onClick = nextClick;
-		if (num >= xWord.length-1){
-			myButtonGroup.children[1].enabled = false;
-		}
-		var mySkippButton = myButtonGroup.add("button",undefined,"Skip word");
-		mySkippButton.onClick = skippClick;
-		myButtonGroup.add("button",undefined,"OK");
-		myButtonGroup.add("button",undefined,"Cancel");
-	
-		myInputWindow.layout.layout (true);
-	
-		if (myInputWindow.show()==1){
-			return xWord[num];
+	var myButtonGroup = myInputWindow.add("group");
+	myButtonGroup.alignment = "right";
+	var myPrevButton = myButtonGroup.add("button",undefined,"Find Previous");
+	myPrevButton.onClick = prevClick;
+	if (count==0){
+		myButtonGroup.children[0].enabled = false;
+	}
+	var myNextButton = myButtonGroup.add("button",undefined,"Find Next");
+	myNextButton.onClick = nextClick;
+	if (count >= found.length-1){
+		myButtonGroup.children[1].enabled = false;
+	}
+	var mySkippButton = myButtonGroup.add("button",undefined,"Skip word");
+	mySkippButton.onClick = skippClick;
+	myButtonGroup.add("button",undefined,"OK");
+	myButtonGroup.add("button",undefined,"Cancel");
+
+	myInputWindow.layout.layout (true);
+
+	if (myInputWindow.show()==1){
+		if (buttonPressed != ""){
+			return buttonPressed;
 		} else {
-			exit();
+			return found[count];
 		}
-		
-		function prevClick(){
-			xWord[num] = "PREVWORD";
-			myInputWindow.close(1);
-		}
+	} else {
+		exit();
+	}
+	
+	function prevClick(){
+		buttonPressed = "PREVWORD";
+		myInputWindow.close(1);
+	}
 
-		function nextClick(){
-			xWord[num] = "NEXTWORD";
-			myInputWindow.close(1);
-		}
+	function nextClick(){
+		buttonPressed = "NEXTWORD";
+		myInputWindow.close(1);
+	}
 
-		function skippClick(){
-			xWord[num] = "SKIPWORD";
-			myInputWindow.close(1);
-		}
+	function skippClick(){
+		buttonPressed = "SKIPWORD";
+		myInputWindow.close(1);
+	}
 }
 
 function createReferenceDestination(referenceWord, name){
+	var referenceName = name;
 	try {
-		if (referenceWord.toString() == "[object Word]"){
-			if (!myDoc.hyperlinkTextDestinations.itemByName(name).isValid){
-				var hypTextDest = myDoc.hyperlinkTextDestinations.add(referenceWord);
-				hypTextDest.name = name;
-				return hypTextDest;
+		var increment = 1;
+		var escape = false;
+		while (escape === false){
+			if (!myDoc.hyperlinkTextDestinations.itemByName(referenceName).isValid){
+				var referenceDestination = myDoc.hyperlinkTextDestinations.add(referenceWord);
+				referenceDestination.name = referenceName;
+				escape = true;
 			} else {
-				message += "\nhyperlink destination with name: " + name + " already exists... skipping";
+				referenceName += increment;
+				increment++;
 			}
-		} else {
-			alert("word did not return as word object");
 		}
 	} catch (e) {
 		alert("create reference destination failed try");
 	}
+	return referenceDestination;
 }
 
-function hyperlinkGlossaryTerms(glossWord, refDest){
-	var hyperlinkSource = myDoc.hyperlinkTextSources.add(glossWord);
-	var hyperlinkDestination = refDest;
-	var name = glossWord.contents + "_gl";
-	var message = "";
-
-	var myHyperlink = myDoc.hyperlinks.add(hyperlinkSource, hyperlinkDestination);
+function hyperlinkGlossaryTerms(glossaryWord, referenceDestination, overWriteStylesBool){
 	try {
-		glossWord.appliedCharacterStyle = "hyperlink_glossary";
-	} catch (e) {
-		alert("unable to apply character style to hyperlink");
-	}
-	try {
-		if (!myDoc.hyperlinks.itemByName(name).isValid){
-			myHyperlink.name = name;
-		} else {
-			message += "\nhyperlink with name: " + name + " already exists... naming hyperlink " + myHyperlink.name;
+		var hyperlinkSource = myDoc.hyperlinkTextSources.add(glossaryWord);
+		var hyperlinkDestination = referenceDestination;
+		var name = glossaryWord.contents + "_gl";
+		var myHyperlink = myDoc.hyperlinks.add(hyperlinkSource, hyperlinkDestination);
+		if (overWriteStylesBool){
+			try {
+				if (myDoc.characterStyles.item("hyperlink_glossary").isValid){
+					glossaryWord.appliedCharacterStyle = "hyperlink_glossary";
+				} else {
+					var hyperlinkGlossaryStyle = myDoc.characterStyles.add();
+					hyperlinkGlossaryStyle.name = "hyperlink_glossary";
+					glossaryWord.appliedCharacterStyle = "hyperlink_glossary";
+				}
+			} catch (e) {
+				alert("unable to apply character style to hyperlink");
+			}
 		}
-	} catch (e) {}
-	if (message!=""){
-		alert(message);
+		var increment = 1;
+		var escape = false;
+		while (escape === false){
+			if (!myDoc.hyperlinks.itemByName(name).isValid){
+				myHyperlink.name = name;
+				escape = true;
+			} else {
+				name += increment;
+			}
+		}
+	} catch (e) {
+		alert("This glossary word is already hyperlinked");
 	}	
 }
 
-function hyperlinkReferenceTerms(refWord, glosDest){
-	var hyperlinkSource = myDoc.hyperlinkTextSources.add(refWord);
-	var hyperlinkDestination = glosDest;
-	var name = refWord.contents;
-	var message = "";
-
-	var myHyperlink = myDoc.hyperlinks.add(hyperlinkSource, hyperlinkDestination);
+function hyperlinkReferenceTerms(referenceWord, glossDestination, overWriteStylesBool){
 	try {
-		refWord.appliedCharacterStyle = "hyperlink_glossary";
-	} catch (e) {
-		alert("unable to apply character style to hyperlink");
-	}
-	try {
-		if (!myDoc.hyperlinks.itemByName(name).isValid){
-			myHyperlink.name = name;
-		} else {
-			message += "\nhyperlink with name: " + name + " already exists... naming hyperlink " + myHyperlink.name;
+		var hyperlinkSource = myDoc.hyperlinkTextSources.add(referenceWord);
+		var hyperlinkDestination = glossDestination;
+		var name = referenceWord.contents;
+		var myHyperlink = myDoc.hyperlinks.add(hyperlinkSource, hyperlinkDestination);
+		if (overWriteStylesBool){
+			try {
+				if (myDoc.characterStyles.item("hyperlink_keyword").isValid){
+					referenceWord.appliedCharacterStyle = "hyperlink_keyword";
+				} else {
+					var hyperlinkKeywordStyle = myDoc.characterStyles.add();
+					hyperlinkKeywordStyle.name = "hyperlink_keyword";
+					referenceWord.appliedCharacterStyle = "hyperlink_keyword";
+				}
+			} catch (e) {
+				alert("unable to apply character style to hyperlink");
+			}
 		}
-	} catch (e) {}
-	if (message!=""){
-		alert(message);
+		var increment = 1;
+		var escape = false;
+		while (escape === false){
+			if (!myDoc.hyperlinks.itemByName(name).isValid){
+				myHyperlink.name = name;
+				escape = true;
+			} else {
+				name += increment;
+			}
+		}
+	} catch (e) {
+		alert("This reference word is already hyperlinked");
 	}
 }
